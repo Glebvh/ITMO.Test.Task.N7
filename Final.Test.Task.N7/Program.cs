670ef7cc5b325c0762548bee2257ee5cefdd7340
glebv <glebv@LAPTOP-54GSHBB5>
2022-04-14 18:51:13 +0300

    Add project files.

---
+++ Final.Test.Task.N7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.Test.Task.N7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            do
+            {
+                Console.WriteLine("Введите значение n");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            while (n <= 0);
+            
+            double S = 0;
+            int i = 1;
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Введите значение D");
+                double D = Convert.ToInt32(Console.ReadLine());
+                S = S + D;
+            }
+
+            double Mid = S/(i - 1);
+            Console.WriteLine("Результат = " + Mid);
+
+            Console.WriteLine("anykey");
+            string y = Console.ReadLine();
+
+        }
+    }
+}
